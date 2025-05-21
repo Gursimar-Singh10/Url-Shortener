eff8f8180cca9d52e38e21088c31b0b192d44169
Gursimar-Singh10 <gursimarsingh1031@gmail.com>
2025-05-20 22:33:02 -0700

    Update Css

--- FRONTEND/src/pages/AuthPage.jsx
+++ FRONTEND/src/pages/AuthPage.jsx
@@ -1,57 +1,3 @@
-// import React, { useState } from 'react'
-// import LoginForm from '../components/LoginForm'
-// import RegisterForm from '../components/RegisterForm'
-
-// const AuthPage = () => {
-
-//     const [login, setLogin] = useState(true)
-
-//     return (
-//         <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-//             {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
-//         </div>
-//     )
-// }
-
-// export default AuthPage
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import LoginForm from '../components/LoginForm';
-// import RegisterForm from '../components/RegisterForm';
-
-// const AuthPage = () => {
-//   const [login, setLogin] = useState(true);
-
-//   return (
-//     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-//       {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
-//     </div>
-//   );
-// };
-
-// export default AuthPage;
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
@@ -61,7 +7,7 @@ const AuthPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-6 small-screen-padding">
-      <div className="bg-white p-10 rounded-xl shadow-lg w-full max-w-md small-screen-padding small-screen-width-full">
+      <div className="bg-white p-2 rounded-xl shadow-lg w-full max-w-md small-screen-padding small-screen-width-full">
         {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
       </div>
     </div>
